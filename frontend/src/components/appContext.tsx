b0bd48e00ff155c905d1b977a65a7d3c21811ed5
Hamid Mayeli <hamid.mayeli@yahoo.com>
2024-08-19 21:50:28 +0100

    Add logout

--- frontend/src/components/appContext.tsx
+++ frontend/src/components/appContext.tsx
@@ -1,48 +1,47 @@
-import { createContext, useState, useMemo, useCallback } from "react";
-import { hookHack } from "./hookHack";
-import { loadCurrentUser, removeCurrentUser, saveCurrentUser } from "tools/userStorage";
-import { logoutApi } from "apis/user";
-
-export const AppContext = createContext({} as {
-    currentUser?: ICurrentUser,
-    login: (user: ICurrentUser) => void,
-    logout: () => void,
-});
-
-export default function AppContextProvider({
-    children,
-}: {
-    children: React.ReactNode
-}) {
-    const [currentUser, setCurrentUser] = useState(loadCurrentUser());
-
-    const login = useCallback((user: ICurrentUser) => {
-        saveCurrentUser(user);
-        setCurrentUser(user);
-    }, []);
-
-    const logout = useCallback(() => {
-        logoutApi()
-            .then(() => {
-                removeCurrentUser();
-                setCurrentUser(undefined);
-            })
-            .catch(() => { });
-    }, []);
-
-    const contextValue = useMemo(() => ({
-        currentUser,
-        login,
-        logout,
-    }), [currentUser, login, logout]);
-
-    hookHack.logout = contextValue.logout;
-    hookHack.login = contextValue.login;
-
-    return (
-        <AppContext.Provider value={contextValue}>
-            {children}
-        </AppContext.Provider>
-    );
-}
+import { createContext, useState, useMemo, useCallback } from "react";
+import { hookHack } from "./hookHack";
+import { loadCurrentUser, removeCurrentUser, saveCurrentUser } from "tools/userStorage";
+import { logoutApi } from "apis/user";
 
+export const AppContext = createContext({} as {
+    currentUser?: ICurrentUser,
+    login: (user: ICurrentUser) => void,
+    logout: () => void,
+});
+
+export default function AppContextProvider({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    const [currentUser, setCurrentUser] = useState(loadCurrentUser());
+
+    const login = useCallback((user: ICurrentUser) => {
+        saveCurrentUser(user);
+        setCurrentUser(user);
+    }, []);
+
+    const logout = useCallback(() => {
+        logoutApi()
+            .then(() => {
+                removeCurrentUser();
+                setCurrentUser(undefined);
+            })
+            .catch(() => { });
+    }, []);
+
+    const contextValue = useMemo(() => ({
+        currentUser,
+        login,
+        logout,
+    }), [currentUser, login, logout]);
+
+    hookHack.logout = contextValue.logout;
+    hookHack.login = contextValue.login;
+
+    return (
+        <AppContext.Provider value={contextValue}>
+            {children}
+        </AppContext.Provider>
+    );
+}
