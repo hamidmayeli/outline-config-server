5fb530d132f74e2c25604f953fae34de004fbff3
Hamid Mayeli <33150399+hamidmayeli@users.noreply.github.com>
2024-08-24 07:07:46 +0100

    Update KeyService.cs

--- backend/API/Services/KeyService.cs
+++ backend/API/Services/KeyService.cs
@@ -20,6 +20,11 @@ public class KeyService(
         var server = _database.FindServerLocally(userId, serverid);
         var client = _outlineServerClientFactory.Create(server.ApiUrl);
 
+        if (request.Limit.Bytes <= 1000_000_000)
+        {
+            request.Limit.Bytes = null;
+        }
+
         var result = await client.CreateKey(server.ApiPrefix, request);
         result.DataLimit.Bytes = request.Limit.Bytes;
 
