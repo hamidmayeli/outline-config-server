a1e2dd08ad2eb4d3a6e127a1ae2882f43bf4377e
Hamid Mayeli <hamid.mayeli@yahoo.com>
2025-12-02 09:39:24 +0000

    Remove Cypress from project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import istanbul from 'vite-plugin-istanbul';
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
@@ -20,13 +19,6 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       tsconfigPaths(),
-      istanbul({
-        include: 'src/*',
-        exclude: ['node_modules'],
-        extension: [ '.js', '.ts', '.jsx', '.tsx',  ],
-        requireEnv: !process.env.CYPRESS,
-        checkProd: true,
-      }),
       VitePWA({ 
         registerType: 'prompt',
         injectRegister: 'auto',
