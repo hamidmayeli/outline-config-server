9cc8259acab8b0ecbe232d66f7c13e36d40b6b5b
Hamid Mayeli <hamid.mayeli@yahoo.com>
2025-12-22 12:03:02 +0000

    Improve formatXAxis in reports

--- frontend/src/pages/Report.tsx
+++ frontend/src/pages/Report.tsx
@@ -118,7 +118,7 @@ export default function Report() {
         const day = String(date.getDate()).padStart(2, '0');
         const hours = String(date.getHours()).padStart(2, '0');
         const minutes = String(date.getMinutes()).padStart(2, '0');
-        return `${year}-${month}-${day} ${hours}:${minutes}`;
+        return hours === "00" ? `${year}-${month}-${day}` : `${hours}:${minutes}`;
     };
 
     const formatYAxis = (tickItem: number) => toHumanReadableBytes(tickItem) ?? "";
